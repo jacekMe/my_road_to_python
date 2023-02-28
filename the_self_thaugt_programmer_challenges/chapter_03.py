b6e86d263daaee07f6dcc19b2707f79727529c73
Jacek Meres <jacekmeres@gmail.com>
2023-02-28 23:56:33 +0100

    add chapter_03.py

--- the_self_thaugt_programmer_challenges/chapter_03.py
+++ the_self_thaugt_programmer_challenges/chapter_03.py
@@ -8,7 +8,7 @@ print("Nice to meet you!:)")
 ## Write a program that prints a message if a variable is less than 10, and a different message is the variable is
 ## greater than or equal to 10
 
-x = 10
+x = 8
 
 if x < 10:
     print("x is less than 10")
